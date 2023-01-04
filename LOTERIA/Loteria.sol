5cef6daeda201a8183ccf59865ed05dccca80c28
echelr73 <eche_lr73@hotmail.com>
2023-01-04 14:53:29 -0300

    Declaraciones Iniciales2

--- LOTERIA/Loteria.sol
+++ LOTERIA/Loteria.sol
@@ -6,4 +6,20 @@ import "./ERC20.sol";
 
 contract Loteria{
 
+    //Instancia del contrato Token
+    ERC20Basic private token;
+
+    //Direcciones
+    address public owner;
+    address public contrato;
+
+    //Numero de tokens a crear
+    uint public tokens_creados = 10000;
+
+    constructor() public {
+        token = new ERC20Basic(tokens_creados);
+        owner = msg.sender;
+        contrato = address(this);
+    }
+
 }
